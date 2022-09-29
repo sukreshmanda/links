bc4a1726851c472ee26c610350848bed64780b5e
sukreshmanda <mandasukresh@gmail.com>
2022-09-29 15:18:40 +0530

    Initial commit

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -10,20 +10,28 @@ function HomeComponent(){
     return(
         <div>
             {df.map(item => {
-                return(<div key={item["link"]}>
-                    <Typography 
-                    variant="h6"
-                    to="/"
-                    sx={{
-                        mr: 2,
-                        to:"/",
-                        fontFamily: 'monospace',
-                        fontWeight: 900,
-                        color: 'inherit',
-                        textDecoration: 'none',
-                    }}>
-                        <Link to={item["link"]} >{item["name"]}</Link>
-                    </Typography>
+                return(
+                <div className="item" style={{justifyContent: "center", display: "flex"}}>
+                    <div className="item-inside">
+                        <div key={item["link"]}>
+                            <p 
+                            variant="h6"
+                            to="/"
+                            sx={{
+                                mr: 2,
+                                to:"/",
+                                fontFamily: 'monospace',
+                                fontWeight: 900,
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}>
+                                <Link to={item["link"]} >{item["name"]}</Link>
+                            </p>
+                        </div>
+                        <Link to={item["link"]}>
+                            <img src={item["image"]} alt={item["Amazon Associates"]} width="100vw" height="100vw"/>
+                        </Link>
+                    </div>
                 </div>
                 );
             })}
--- src/data.js
+++ src/data.js
@@ -7,12 +7,16 @@ function data(){
         {
             "name": "Amazon Affiliate Link Generator",
             "link": "/amazon-affiliate",
+            "image": "https://thesbjournal.com/wp-content/uploads/2017/10/Amazon-1-1200x545_c.png",
+            "image-alt" : "Amazon Associates",
             "isProductionReady": false,
             "component": AmazonAffiliateComponent
         },
         {
             "name": "Youtube",
             "link": "/youtube",
+            "image": "https://www.gstatic.com/youtube/img/branding/youtubelogo/svg/youtubelogo.svg",
+            "image-alt" : "Youtube Channel",
             "isProductionReady": false,
             "component": YoutubeComponent
         },
